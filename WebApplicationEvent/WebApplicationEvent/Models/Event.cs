93771c326180e42699caba740c98daf728596565
Viviana Osorio <v.osorio@3sixtydutyfree.com>
2025-07-04 11:00:14 -0500

    Add Api Swagger Get,, create, update and delete

---
+++ WebApplicationEvent/WebApplicationEvent/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationEvent.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+}
---
+++ WebApplicationEvent/WebApplicationEvent/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplicationEvent.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApplicationEventContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApplicationEventContext") ?? throw new InvalidOperationException("Connection string 'WebApplicationEventContext' not found.")));
+
+// Agregar servicios al contenedor
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Event API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Event API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
